29f38f046ee1ae97a196318e31d89bd94ac4c4f0
August Lilleaas <augustlilleaas@gmail.com>
2009-01-30 06:25:24 +0100

    Using form_for(obj, :live_validations => true) instead of live_validations_form_for(obj).

--- lib/live_validations/view_helpers.rb
+++ lib/live_validations/view_helpers.rb
@@ -1,23 +1,30 @@
 module LiveValidations
   module ViewHelpers
-    def live_validation_form_for(record_name_or_array, *args, &block)
+    def self.included(base)
+      base.alias_method_chain :form_for, :live_validations
+    end
+    
+    def form_for_with_live_validations(record_name_or_array, *args, &block)
       options = args.extract_options!
-      options.merge!(:builder => LiveValidations::FormBuilder)
       
-      record = case record_name_or_array
-      when Array
-        array.last
-      when ActiveRecord::Base
-        record_name_or_array
+      if options[:live_validations]
+        options.merge!(:builder => LiveValidations::FormBuilder)
+        
+        record = case record_name_or_array
+        when Array
+          array.last
+        when ActiveRecord::Base
+          record_name_or_array
+        else
+          raise ArgumentError, 'live_validation_form_for only supports an array (e.g. [:admin, @post]) or an active record instance (e.g. @post) as its first argument.'
+        end
+
+        self.adapter_instance = LiveValidations.current_adapter.new(record)
+        form_for_without_live_validations(record_name_or_array, *args, &block)
+        concat(%{<script type="text/javascript">#{adapter_instance.render_json}</script>}, block.binding) if adapter_instance.utilizes_json?
       else
-        raise ArgumentError, 'live_validation_form_for only supports an array (e.g. [:admin, @post]) or an active record instance (e.g. @post) as its first argument.'
+        form_for_without_live_validations(record_name_or_array, *args, &block)
       end
-      
-      self.adapter_instance = LiveValidations.current_adapter.new(record)
-            
-      form_for(record_name_or_array, *(args << options), &block)
-      
-      concat(%{<script type="text/javascript">#{adapter_instance.render_json}</script>}, block.binding) if adapter_instance.utilizes_json?
     end
     
     def adapter_instance=(record)
