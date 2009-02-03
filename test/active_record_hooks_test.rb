e1cc6c0a99f36297f0292f27746b589ba1f6a0e6
August Lilleaas <augustlilleaas@gmail.com>
2009-02-03 10:32:24 +0100

    DRY-ing the active record hooks test.

--- test/active_record_hooks_test.rb
+++ test/active_record_hooks_test.rb
@@ -10,23 +10,16 @@ class ActiveRecordHooksTest < Test::Unit::TestCase
   end
   
   def test_validation_callbacks_for_on_new_record
-    post = new_post
-    Post.validates_presence_of :title
-    Post.validates_length_of :title, :maximum => 50
-    # This one is ignored, as it's on update, and the post is a new record.
-    Post.validates_format_of :body, :with => /silly/, :on => :update
-
-    validators = post.validation_callback_for(:title)
-    assert_equal 2, validators.size
-    
-    assert validators.detect {|v| v.options[:validation_method] == :length }
-    assert validators.detect {|v| v.options[:validation_method] == :presence }
-    
-    assert !post.validation_callback_for(:body).detect {|v| v.options[:validation_method] == :format}
+    assert_callback_hooks(new_post)
   end
   
   def test_validation_callbacks_for_on_existing_record
-    post = create_post
+    assert_callback_hooks(create_post)
+  end
+  
+  private
+  
+  def assert_callback_hooks(post)
     Post.validates_presence_of :title
     Post.validates_length_of :title, :maximum => 50
     # This one is ignored, as it's on update, and the post is a new record.
@@ -40,8 +33,6 @@ class ActiveRecordHooksTest < Test::Unit::TestCase
     
     assert !post.validation_callback_for(:body).detect {|v| v.options[:validation_method] == :format}
   end
-  
-  private
   
   def new_post
     Post.new(:title => "Yarr", :excerpt => "Yep", :body => "Yap")
