916ff9131dc6142bbff519d02ece104e59c84ca1
August Lilleaas <augustlilleaas@gmail.com>
2009-02-05 19:52:20 +0100

    Adding length (min and max, not range and is) and numericality to the validatious adapter.

--- lib/live_validations/adapters/validatious.rb
+++ lib/live_validations/adapters/validatious.rb
@@ -7,9 +7,12 @@ module LiveValidations
       end
   
       validates :length do |v|
+        v.tag_attributes[:class] = "min-length #{v.callback.options[:minimum]}" if v.callback.options[:minimum]
+        v.tag_attributes[:class] = "max-length #{v.callback.options[:maximum]}" if v.callback.options[:maximum]
       end
   
       validates :numericality do |v|
+        v.tag_attributes[:class] = "numeric"
       end
   
       validates :confirmation do |v|
