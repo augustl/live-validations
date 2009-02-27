c24d14519c25314d747b4352af0bee2a3238909d
August Lilleaas <augustlilleaas@gmail.com>
2009-02-27 20:33:47 +0100

    Adding some more validations to the livevalidation.com adapter.

--- lib/live_validations/adapters/livevalidations_dot_com.rb
+++ lib/live_validations/adapters/livevalidations_dot_com.rb
@@ -16,6 +16,43 @@ module LiveValidations
         }
       end
       
+      validates :numericality do |v, attribute|
+        v.data["validators"] << %{
+          var validator = new LiveValidation('#{v.prefix}_#{attribute}');
+          validator.add(Validate.Numericality, {onlyInteger: true, failureMessage: "#{v.message_for(:not_a_number)}"})
+        }
+      end
+      
+      validates :length do |v, attribute|
+        if v.callback.options[:minimum]
+          v.data["validators"] << %{
+            var validator = new LiveValidation('#{v.prefix}_#{attribute}');
+            validator.add(Validate.Length, {true, minimum: #{v.callback.options[:minimum]}});
+          }
+        end
+        
+        if v.callback.options[:maximum]
+          v.data["validators"] << %{
+            var validator = new LiveValidation('#{v.prefix}_#{attribute}');
+            validator.add(Validate.Length, {true, maximum: #{v.callback.options[:maximum]}});
+          }
+        end
+        
+        if v.callback.options[:within]
+          v.data["validators"] << %{
+            var validator = new LiveValidation('#{v.prefix}_#{attribute});
+            validator.add(Validate.Length, {minimum: #{v.callback.options[:within].first}, maximum: #{v.callback.options[:within].last}});
+          }
+        end
+        
+        if v.callback.options[:is]
+          v.data["validators"] << %{
+            var validator = new LiveValidation('#{v.prefix}_#{attribute}');
+            validator.add(Validate.Length, {is: #{v.callback.options[:is]}});
+          }
+        end
+      end
+      
       json do |a|
         a.data["validators"].join
       end
--- test/live_validations_dot_com_test.rb
+++ test/live_validations_dot_com_test.rb
@@ -20,6 +20,31 @@ class LiveValidationsDotComTest < ActiveSupport::TestCase
     assert_json_output "Validate.Format", "/ohai/"
   end
   
+  def test_numericality
+    Post.validates_numericality_of :age
+    assert_json_output "Validate.Numericality"
+  end
+
+  def test_length_with_minimum
+    Post.validates_length_of :title, :minimum => 10
+    assert_json_output "Validate.Length", "minimum: 10"
+  end
+  
+  def test_length_with_maximum
+    Post.validates_length_of :title, :maximum => 10
+    assert_json_output "Validate.Length", "maximum: 10"
+  end
+  
+  def test_length_with_range
+    Post.validates_length_of :title, :within => 10..20
+    assert_json_output "Validate.Length", "minimum: 10", "maximum: 20"
+  end
+  
+  def test_length_with_is
+    Post.validates_length_of :title, :is => 20
+    assert_json_output "Validate.Length", "is: 20"
+  end
+  
   def assert_json_output(*outputs)
     validator = LiveValidations.current_adapter.new(Post.new)
     outputs.each {|o| assert validator.data['validators'][0].include?(o) }
